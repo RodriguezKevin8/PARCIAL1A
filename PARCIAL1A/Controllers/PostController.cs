ae5e773855a74b17d7132e72fc76e677ba90ef31
Carlos Yanez <carlitosyanez9@gmail.com>
2024-03-09 14:48:15 -0600

    postController

---
+++ PARCIAL1A/Controllers/PostController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PARCIAL1A.Models;
+
+namespace PARCIAL1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly Parcial1aContext _parcial1aContexto;
+        public PostController(Parcial1aContext parcial1aContexto)
+        {
+            _parcial1aContexto = parcial1aContexto;
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<Post> listadoPost = _parcial1aContexto.Posts.ToList();
+            if (listadoPost.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoPost);
+        }
+
+        [HttpGet]
+        [Route("get{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var post = await _parcial1aContexto.Posts.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Post(Post post)
+        {
+            try
+            {
+                _parcial1aContexto.Posts.Add(post);
+                await _parcial1aContexto.SaveChangesAsync();
+
+                return Ok(post);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);  
+            }
+            
+        }
+        
+
+        [HttpPut]
+        [Route("acrtuaizar")]
+
+        public async Task<IActionResult> Put(int id, Post postactualizar)
+        {
+            var post=_parcial1aContexto.Posts.Find(id);
+            if (post == null) {
+                return NotFound();
+            }
+
+            post.Titulo = postactualizar.Titulo;
+            post.Contenido= postactualizar.Contenido;
+            post.Fechapublicacion = postactualizar.Fechapublicacion;
+            post.Autorid = postactualizar.Autorid;
+
+            _parcial1aContexto.Entry(post).State = EntityState.Modified;
+            _parcial1aContexto.SaveChanges();
+
+            return Ok(post);
+        }
+        private bool PostExists(int id)
+        {
+            return _parcial1aContexto.Posts.Any(e => e.Postid == id);
+        }
+
+        [HttpDelete]
+        [Route("eliminar{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _parcial1aContexto.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            _parcial1aContexto.Posts.Remove(post);
+            await _parcial1aContexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+    }
+}
